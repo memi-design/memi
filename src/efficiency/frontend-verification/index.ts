1eab4ffe8021664a060424a2b390dc3d2ce8f465
sarveshsea <sarveshjax@gmail.com>
2026-08-06 10:17:34 -0500

    feat: add fail-closed web verification adapter

---
+++ src/efficiency/frontend-verification/index.ts
@@ -0,0 +1,10 @@
+export * from "./web-adapter.js";
+export type {
+  WebArtifactKind,
+  WebColorScheme,
+  WebReducedMotion,
+  WebUiState,
+  WebVerificationRequirement,
+  WebVerificationRequirementId,
+  WebViewport,
+} from "./web-contract.js";
